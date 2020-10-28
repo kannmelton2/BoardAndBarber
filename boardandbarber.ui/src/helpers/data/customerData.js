9e07da1eec34e31d36141bc4c21a0c0bb8c57033
Kayla Melton <kannmelton2@gmail.com>
2020-10-27 17:14:54 -0700

    added local database connection

--- boardandbarber.ui/src/helpers/data/customerData.js
+++ boardandbarber.ui/src/helpers/data/customerData.js
@@ -1,21 +1,10 @@
+import axios from 'axios';
+import {baseUrl} from '../constants.json';
 
 const getCustomers = () => new Promise((resolve, reject) => {
-    resolve([
-        {
-            id: 1,
-            name: "nathan",
-            birthday: "5/27/1985",
-            favoriteBarber: "Jim",
-            notes: "High and tight"
-        },
-        {
-            id: 2,
-            name: "Bill",
-            birthday: "4/5/1999",
-            favoriteBarber: "Karl",
-            notes: "short on the sides and long on top"
-        }
-    ])
+   axios.get(`${baseUrl}/customers`)
+   .then(response => resolve(response.data))
+   .catch(err => reject(err));
 });
 
 export default { getCustomers };
